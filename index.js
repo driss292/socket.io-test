daab505a14c9b4b30e4f63b083961aae1d0f174e
driss292 <drisskaci@outlook.fr>
2022-06-13 10:00:38 +0200

    OK

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const http = require("http").Server(app);
+// const server = http.createServer(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
@@ -8,8 +9,15 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log(`Un utilisateur s'est connecté`);
+  socket.on("disconnect", () => {
+    console.log(`Un utilisateur s'est déconnecté`);
+  });
+  socket.on("chat message", (msg) => {
+    console.log(`Message recu : ${msg}`);
+    io.emit("chat message", msg);
+  });
 });
 
-server.listen(3000, () => {
+http.listen(3000, () => {
   console.log(`server OK...`);
 });
